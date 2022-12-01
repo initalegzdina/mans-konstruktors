c6a240efb9698d41e783e6049a951556e56e3021
ilegzdina <initakula@gmail.com>
2022-12-01 20:38:25 +0000

    Test file started for naked.py

---
+++ test_naked.py
@@ -0,0 +1,14 @@
+from naked import *
+
+print("Asteroid code test")
+print("----------")
+print("Testing function: \"mysql_insert_ast_into_db\"")
+# Testing passing 
+print(" [] -->")
+
+assert mysql_insert_ast_into_db == []
+print("OK")
+print("----------")
+
+print("Asteroid code test -> ALL OK")
+print("----------------------------------------")
